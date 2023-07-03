9aa557b192be98704349c379a2a2a8335afb5ca6
Brian Phiri <brianphiri.allnetzambia@gmail.com>
2023-07-03 20:46:30 +0200

    On branch master Your branch is ahead of 'origin/master' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   app/controllers/classes_form.php
    	modified:   app/views/classes.php
    	renamed:    app/views/class_form.php -> app/views/classes_form.php

--- app/controllers/classes_form.php
+++ app/controllers/classes_form.php
@@ -2,10 +2,46 @@
 
 Class Classes_form extends Controller
 {
-	function index()
-	{
+	function index(){
 		$data['page_title'] = "Classes Form";
-		$this->view("class_form",$data);
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['save_class'])) {
+            $this->handleClassForm();
+        }
+
+		$this->view("classes_form",$data);
 	}
+
+    public function handleClassForm()
+    {
+        $className = $_POST['class_name'];
+        $experienceRequired = $_POST['experience_required'];
+
+        $this->saveClassToDatabase($className, $experienceRequired);
+    }
+
+    public function saveClassToDatabase($className, $experienceRequired)
+    {
+        $db = new Database(); // Instantiate your database class
+
+        // Prepare the SQL query
+        $query = "INSERT INTO classes (class_name, experience_required) VALUES (?, ?)";
+
+        // Bind the parameter values
+        $values = array($className, $experienceRequired);
+
+        // Execute the query using the write() method of the Database class
+        if ($db->write($query, $values)) {
+            // Data inserted successfully
+            echo "<script>alert('Class Added');
+                location=('http://localhost/membership/public/classes');
+                </script>";
+        } else {
+            // Error occurred while inserting data
+            echo "<script>alert('Some Error Occured');
+                location=('http://localhost/membership/public/classes_form');
+                </script>";
+        }
+    }
 
 }
